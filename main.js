c4b4582bcaeeead4cd7d66c64e60383e344bc7d1
zfelice <zachfelice@aol.com>
2021-04-03 23:50:32 -0400

    progress bar syncing

--- main.js
+++ main.js
@@ -1,36 +1,40 @@
 var wcLoop;
 var xp = 0;
-var interval;
+
 
 function update(id, content) {
     document.getElementById(id).innerHTML = content;
 }
 
 function cutWood() {
+    const myBar = document.getElementById('myBar');
     if (wcLoop) {
         clearInterval(wcLoop);
         wcLoop = null;
     } else {
         wcLoop = setInterval(() => {
             xp++;
+            myBar.style.width = xp + '%';
+            myBar.innerHTML = xp + '%';
             update("xp", `${xp} Woodcutting Experience`);
         }, 3000);
     }
-
-    const myBar = document.getElementById('myBar');
-    var width = 0;
-    var timeInt = setInterval(frame, 3000);
-    //wc progress bar function
-    function frame() {
-        if (width >= 100) {
-            clearInterval(timeInt);
-        } else {
-            width++;
-            myBar.style.width = width + '%';
-            myBar.innerHTML = width + '%';
-        }
-    }
 }
+
+/*   const myBar = document.getElementById('myBar');
+   var width = 0;
+   var timeInt = setInterval(frame, 3000);
+   //wc progress bar function
+   function frame() {
+       if (width >= 100) {
+           clearInterval(timeInt);
+       } else {
+           width++;
+           myBar.style.width = width + '%';
+           myBar.innerHTML = width + '%';
+       }
+   }
+*/
 
 //cutWood();
 
