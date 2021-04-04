bdc723aa3dc64677c3340afea0d1750b07ba2d49
zfelice <zachfelice@aol.com>
2021-04-04 14:06:40 -0400

    added fishing

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+
+//game variables
 var wcLoop;
-var xp = 0;
+var wcXp = 0;
+var fishLoop;
+var fishXp = 0;
 
 
 function update(id, content) {
@@ -7,24 +11,40 @@ function update(id, content) {
 }
 
 function cutWood() {
-    const myBar = document.getElementById('myBar');
+    const wcBar = document.getElementById('wcBar');
     if (wcLoop) {
         clearInterval(wcLoop);
         wcLoop = null;
     } else {
         wcLoop = setInterval(() => {
-            xp++;
-            myBar.style.width = xp + '%';
-            myBar.innerHTML = xp + '%';
-            update("xp", `${xp} Woodcutting Experience`);
+            wcXp++;
+            wcBar.style.width = wcXp + '%';
+            wcBar.innerHTML = wcXp + '%';
+            update("wcXp", `${wcXp} Woodcutting Experience`);
         }, 3000);
     }
 }
 
+function catchFish() {
+    const fishBar = document.getElementById('fishBar');
+    if (fishLoop) {
+        clearInterval(fishLoop);
+        fishLoop = null;
+    } else {
+        fishLoop = setInterval(() => {
+            fishXp++;
+            fishBar.style.width = fishXp + '%';
+            fishBar.innerHTML = fishXp + '%';
+            update("fishXp", `${fishXp} Fishing Experience`);
+        }, 3000);
+    }
+}
+
+//progress bar
 /*   const myBar = document.getElementById('myBar');
    var width = 0;
    var timeInt = setInterval(frame, 3000);
-   //wc progress bar function
+   //progress bar function
    function frame() {
        if (width >= 100) {
            clearInterval(timeInt);
