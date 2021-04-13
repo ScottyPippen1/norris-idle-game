2a3c6475a0fd45faade00a559f247b57802f74c4
zfelice <zachfelice@aol.com>
2021-04-12 20:50:11 -0400

    adding .gitignore

---
+++ entities/constants.js
@@ -0,0 +1,29 @@
+const skillsData = {
+    skills: [
+        {
+            id: 0,
+            name: "Woodcutting",
+            baseIntervalSpeed: 3000
+        },
+        {
+            id: 1,
+            name: "Fishing"
+        },
+        {
+            id: 2,
+            name: "Mining"
+        },
+        {
+            id: 3,
+            name: "Firemaking"
+        },
+        {
+            id: 4,
+            name: "Cooking"
+        },
+        {
+            id: 5,
+            name: "Smithing"
+        },
+    ]
+}
